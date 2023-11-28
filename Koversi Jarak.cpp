2795aa57d698ae46f8718992619de923610944ee
reyenno <152334863+reyenno@users.noreply.github.com>
2023-11-29 04:36:23 +0700

    Update Koversi Jarak.cpp

--- Koversi Jarak.cpp
+++ Koversi Jarak.cpp
@@ -11,17 +11,17 @@ int main(){
 	scanf("%f",&a);
 	
 	e = a * 10;
-	printf("Hasil hm : %.2f hm \n, e")
+	printf("Hasil hm : %.2f hm \n, e");
 	b= a *100;
 	printf("Hasil dam : %.2f dam \n",b);
 	c= a * 1000;
 	printf("Hasil m : %.2f m \n",c);
-	d= a * 10000
-	printf("Hasil dm : %.2f dm \n, d")
-	f= a * 100000
-	printf("Hasil cm : %.2f cm \n, f")
-	g = a * 1000000
-	printf("Hasil mm : %.2f mm \n, g")
+	d= a * 10000;
+	printf("Hasil dm : %.2f dm \n, d");
+	f= a * 100000;
+	printf("Hasil cm : %.2f cm \n, f");
+	g = a * 1000000;
+	printf("Hasil mm : %.2f mm \n, g");
 	system("PAUSE");
 	return 0;
 }
