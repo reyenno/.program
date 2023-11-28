f8312cf2425f355264360d6a38a52b4195b814dd
reyenno <152334863+reyenno@users.noreply.github.com>
2023-11-29 04:35:24 +0700

    Add files via upload

---
+++ Koversi Jarak.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	float a,b,c;
+	
+	printf("============================== \n");
+	printf("Aplikasikan Konversi Jarak \n");
+	printf("============================== \n");
+	printf("Masukan Jarak dalam Satuan KM : ");
+	scanf("%f",&a);
+	
+	e = a * 10;
+	printf("Hasil hm : %.2f hm \n, e")
+	b= a *100;
+	printf("Hasil dam : %.2f dam \n",b);
+	c= a * 1000;
+	printf("Hasil m : %.2f m \n",c);
+	d= a * 10000
+	printf("Hasil dm : %.2f dm \n, d")
+	f= a * 100000
+	printf("Hasil cm : %.2f cm \n, f")
+	g = a * 1000000
+	printf("Hasil mm : %.2f mm \n, g")
+	system("PAUSE");
+	return 0;
+}
